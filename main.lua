84ac9e61dda395858559dc290e2ff941640f6595
Tanner Rogalsky <the.t.dawg@gmail.com>
2012-12-13 12:28:28 -0500

    large refactor

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 do
   function string:split(sep)
-    local sep, fields = sep or ":", {}
-    local pattern = string.format("([^%s]+)", sep)
+    local sep, fields = sep or ':', {}
+    local pattern = string.format('([^%s]+)', sep)
     self:gsub(pattern, function(c) fields[#fields+1] = c end)
     return fields
   end
@@ -9,107 +9,22 @@ do
   local IRC = require 'irc'
   local redis = require 'redis'
   local cron = require 'cron'
-  local json = require 'json'
-  local http = require 'socket.http'
+
+  http = require 'socket.http'
   local socket = require 'socket'
 
-  local db_client = redis.connect('127.0.0.1', 6379)
-  assert(db_client:ping(), "Check redis host and port")
+  db_client = redis.connect('127.0.0.1', 6379)
+  assert(db_client:ping(), 'Check redis host and port')
 
-  local environment = arg[1] or "test"
-  local config = require("config." .. environment .. "_config")
+  local environment = arg[1] or 'test'
+  config = require('config.' .. environment .. '_config')
 
-  local irc_client = IRC.connect("irc.mountai.net")
+  irc_client = IRC.connect('irc.mountai.net')
   irc_client:nick(config.nick)
   irc_client:user('bot', '0', '*', 'baddest man in the whole damn town')
 
-  local replies, commands = {}, {}
-  function commands.help(chan, nick, args)
-    irc_client:privmsg(chan, "!kb register <keyboard>, !kb del, !kb list <user>, !kb listall, !subscribe")
-  end
-  function commands.subscribe(chan, nick, args)
-    db_client:sadd("irc:reddit:subscribed_users", nick)
-    irc_client:privmsg(chan, "Subscribed!")
-  end
-  function commands.unsubscribe(chan, nick, args)
-    db_client:srem("irc:reddit:subscribed_users", nick)
-    irc_client:privmsg(chan, "Unsubscribed!")
-  end
-  function commands.kb(chan, nick, args)
-    local subcommand = table.remove(args, 1)
-    local response = nil
-    if subcommand == "register" and #args > 0 then
-      local keyboard = table.concat(args, " ")
-      db_client:rpush("irc:keyboards:"..nick, keyboard)
-      response = "Registered: " .. keyboard
-    elseif subcommand == "list" then
-      response = table.concat(db_client:lrange("irc:keyboards:"..args[1], 0, -1), ", ")
-    elseif subcommand == "del" then
-      db_client:del("irc:keyboards:"..nick)
-      response = "Removed all keyboards registered to " .. nick
-    elseif subcommand == "listall" then
-      local nicks = db_client:smembers("irc:users:nicks")
-      for _,n in ipairs(nicks) do
-        local keyboards = db_client:lrange("irc:keyboards:"..n, 0, -1)
-        if keyboards and #keyboards > 0 then
-          local kb_text = table.concat(keyboards, ", ")
-          irc_client:privmsg(nick, n.. ": " ..kb_text)
-        end
-      end
-    end
-
-    if response then
-      irc_client:privmsg(chan, response)
-    end
-  end
-
-
-  function replies.ping(prefix, rest)
-    irc_client:pong(rest)
-    db_client:ping()
-
-    local base_url = "http://www.reddit.com"
-    local response, status_code, headers, status = http.request(base_url .. "/r/MechanicalKeyboards/new/.json?limit=10")
-    if status_code == 200 then
-      response = json.decode(response)
-      local posts = response.data.children
-      for i,post_container in ipairs(posts) do
-        local post = post_container.data
-        local new = db_client:hsetnx("irc:reddit:new_posts", post.id, post.created)
-
-        if new then
-          local text = "New post: '" .. post.title:match "^%s*(.-)%s*$" .. "' by " .. post.author
-          text = text .. " @ http://redd.it/" .. post.id
-          for _,nick in ipairs(db_client:smembers("irc:reddit:subscribed_users")) do
-            irc_client:privmsg(nick, text)
-          end
-          irc_client:privmsg(config.channel, text)
-        end
-      end
-    end
-  end
-  function replies.privmsg(prefix, rest)
-    local chan = rest:match('(%S+)')
-    local msg = rest:match(':(.*)')
-    local nick = prefix:match('(%S+)!')
-    local host = prefix:match('@(%S+)')
-    local cmd, args = msg:match('^!(%S+)(.*)')
-    if cmd then
-      cmd = cmd:lower()
-      args = args:split(" ")
-    end
-
-    if not chan:find('^#') then
-      chan = nick
-    end
-
-    db_client:sadd("irc:users:nicks", nick)
-
-    if type(commands[cmd]) == "function" then
-      local success, message = pcall(commands[cmd],chan, nick, args)
-      if not success then print(message) end
-    end
-  end
+  local commands = require 'commands'
+  local replies = require 'replies'
 
   cron.after(3, function()
     irc_client:join(config.channel)
---
+++ replies.lua
@@ -0,0 +1,50 @@
+local replies = {}
+local json = require 'json'
+function replies.ping(prefix, rest)
+  irc_client:pong(rest)
+  db_client:ping()
+
+  local base_url = "http://www.reddit.com"
+  local response, status_code, headers, status = http.request(base_url .. "/r/MechanicalKeyboards/new/.json?limit=10")
+  if status_code == 200 then
+    response = json.decode(response)
+    local posts = response.data.children
+    for i,post_container in ipairs(posts) do
+      local post = post_container.data
+      local new = db_client:hsetnx("irc:reddit:new_posts", post.id, post.created)
+
+      if new then
+        local text = "New post: '" .. post.title:match "^%s*(.-)%s*$" .. "' by " .. post.author
+        text = text .. " @ http://redd.it/" .. post.id
+        for _,nick in ipairs(db_client:smembers("irc:reddit:subscribed_users")) do
+          irc_client:privmsg(nick, text)
+        end
+        irc_client:privmsg("#keybaords", text)
+      end
+    end
+  end
+end
+function replies.privmsg(prefix, rest)
+  local chan = rest:match('(%S+)')
+  local msg = rest:match(':(.*)')
+  local nick = prefix:match('(%S+)!')
+  local host = prefix:match('@(%S+)')
+  local cmd, args = msg:match('^!(%S+)(.*)')
+  if cmd then
+    cmd = cmd:lower()
+    args = args:split(" ")
+  end
+
+  if not chan:find('^#') then
+    chan = nick
+  end
+
+  db_client:sadd("irc:users:nicks", nick)
+
+  if type(commands[cmd]) == "function" then
+    local success, message = pcall(commands[cmd],chan, nick, args)
+    if not success then print(message) end
+  end
+end
+
+return replies
